9cd607ae0b7a86b3986b6b2caf93d2eef92cdaac
DaiYu <840673183@qq.com>
2024-02-05 22:18:57 +0800

    Upload By 呆鱼

---
+++ Assets/web/script.js
@@ -0,0 +1,23 @@
+function TrunSideNav() {
+  if (num % 2 === 0) {
+    document.getElementById("SideNav").style.width = "100vw";
+  } else {
+    document.getElementById("SideNav").style.width = "0";
+  }
+  num++;
+}
+var num = 0;
+LA.init({ id: "KIJVLwHWaiNAjIGG", ck: "KIJVLwHWaiNAjIGG" });
+new LingQue.Monitor().init({ id: "JpvA7imL0NrZzPiQ" });
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "kwkrckpcin");
